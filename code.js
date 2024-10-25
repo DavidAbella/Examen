c050eaa9d93fb9ba4ac307488b88d7dd6784dc91
DavidAbella <david.abella@estudiants.urv.cat>
2024-10-25 10:15:18 +0200

    commit 10:15

--- code.js
+++ code.js
@@ -5,27 +5,37 @@ async function getPokemonList() {
     const data = await response.json();
   }
 
-async function displayPokemonList() {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
-  const data = await response.json();
-  const pokemonList = data.results;
+async function onDisplayPokemonList() {
+  const formElements = form.elements;
+  const searchFormElement = formElements.search;
+  const characterName = searchFormElement.value;
+  searchFormElement.value = '';
 
-  const titleElement = document.createElement('h2');
-  titleElement.textContent = 'Pokédex Kanto';
-  document.body.appendChild(titleElement);
+  displayPokemonList(pokemonName).then(async (pokemon) =>  {
+    if (!character) {
+      alert('pokemon not found');
+      return;
+    }
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
+    const data = await response.json();
+    const pokemonList = data.results;
+  
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = 'Pokédex Kanto';
+    document.body.appendChild(titleElement);
+  
+    const listElement = document.createElement('ul');
+    pokemonList.forEach(pokemon => {
+      const listItem = document.createElement('li');
+      // listItem.textContent = pokemon.name;
+      listItem.textContent = `${pokemon.name} (#${pokemon.url.split('/')[6]})`;
+      listElement.appendChild(listItem);
+    });
+  
+    document.body.appendChild(listElement);
+    return false
+  }
 
-  const listElement = document.createElement('ul');
-  pokemonList.forEach(pokemon => {
-    const listItem = document.createElement('li');
-    // listItem.textContent = pokemon.name;
-    listItem.textContent = `${pokemon.name} (#${pokemon.url.split('/')[6]})`;
-    listElement.appendChild(listItem);
-  });
-
-  document.body.appendChild(listElement);
-
-  const kantoButton = document.getElementById('kantoButton');
-  kantoButton.addEventListener('click', displayPokemonList);
 }
 
 
