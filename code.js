eea68aa7b438a0c1594abdd6a2a8bc26e4a57ce2
DavidAbella <david.abella@estudiants.urv.cat>
2024-10-25 09:00:45 +0200

    commit 9:00

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 const POKEAPI_BASE_URL = 'https://pokeapi.co/';
 
 async function getPokemonList() {
-    const response = await fetch(`${POKEAPI_BASE_URL}/films/`);
+    const response = await fetch(`${POKEAPI_BASE_URL}/pokemon/`);
     const data = await response.json();
-    
+
   }
